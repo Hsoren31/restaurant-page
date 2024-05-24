a7c957ea6bbb23bbc44fc504b0385be835c5063a
Haley Sorensen <sorensenhaley@gmail.com>
2024-05-24 14:52:32 -0600

    Add content to contact page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import {homeHeader, homeImg, homeText} from './home_page';
 import { mainMenu, menuImg, drinkMenu } from './menu_page';
+import { managerInfo, ownerInfo } from './contact_page';
 
 import './style.css';
 
 const homeButton = document.getElementById('home');
 const menuButton = document.getElementById('menu');
-const aboutButton = document.getElementById('about');
+const contactButton = document.getElementById('contact');
 const contentDiv = document.getElementById('content');
+
+contentDiv.append(homeHeader(), homeImg(), homeText());
 
 homeButton.addEventListener('click', () => {
     contentDiv.innerHTML = '';
@@ -19,7 +22,8 @@ menuButton.addEventListener('click', () => {
 
 });
 
-aboutButton.addEventListener('click', () => {
+contactButton.addEventListener('click', () => {
     contentDiv.innerHTML = '';
+    contentDiv.append(managerInfo(), ownerInfo());
 });
 
